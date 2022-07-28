a5cbf6055205cb3063aa037d3e3c616a30eef053
Brandon <darkpuppet_world@hotmail.com>
2022-07-27 19:11:02 -0500

    Data

--- src/main/java/com/test/demoweb/repository/BookModel.java
+++ src/main/java/com/test/demoweb/repository/BookModel.java
@@ -1,8 +1,16 @@
 package com.test.demoweb.repository;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "books")
 public class BookModel {
 
     @Id
@@ -22,60 +30,5 @@ public class BookModel {
     @Column(name = "image")
     private String image;
 
-    //id
-    public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    //code
-    public String getCode() {
-        return code;
-    }
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    //title
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    //author
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    //edition
-    public String getEdition() {
-        return edition;
-    }
-    public void setEdition(String edition) {
-        this.edition = edition;
-    }
-
-    //description
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    //image
-    public String getImage() {
-        return image;
-    }
-    public void setImage(String image) {
-        this.image = image;
-    }
 
 }
